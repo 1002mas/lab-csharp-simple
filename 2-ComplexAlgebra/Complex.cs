055e9f1e82253d2198772b0b88f8e7d1b634c420
Carafassi Samuele <samuele.carafassi@studio.unibo.it>
2021-12-08 15:28:42 +0100

    completed exercise 2.ComplexAlgebra

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,22 +1,51 @@
+using System;
+
 namespace ComplexAlgebra
 {
     /// <summary>
     /// A type for representing Complex numbers.
     /// </summary>
-    ///
-    /// TODO: Model Complex numbers in an object-oriented way and implement this class.
-    /// TODO: In other words, you must provide a means for:
-    /// TODO: * instantiating complex numbers
-    /// TODO: * accessing a complex number's real, and imaginary parts
-    /// TODO: * accessing a complex number's modulus, and phase
-    /// TODO: * complementing a complex number
-    /// TODO: * summing up or subtracting two complex numbers
-    /// TODO: * representing a complex number as a string or the form Re +/- iIm
-    /// TODO:     - e.g. via the ToString() method
-    /// TODO: * checking whether two complex numbers are equal or not
-    /// TODO:     - e.g. via the Equals(object) method
     public class Complex
     {
-        // TODO: fill this class\
+        public readonly double Real;
+        public readonly double Imaginary;
+        public readonly double Modulus;
+        public readonly double Phase;
+
+        public Complex(double real, double imaginary)
+        {
+            Imaginary = imaginary;
+            Real = real;
+            Modulus = Math.Sqrt(Math.Pow(Real, 2) + Math.Pow(Imaginary, 2));
+            Phase = Math.Atan2(Imaginary, Real);
+        }
+
+        public Complex Complement() => new Complex(Real, -Imaginary);
+        
+        public Complex Plus(Complex num) => new Complex(Real + num.Real, Imaginary + num.Imaginary);
+        
+        public Complex Minus(Complex num) => new Complex(Real - num.Real, Imaginary - num.Imaginary);
+
+        private bool Equals(Complex other)
+        {
+            return Real.Equals(other.Real) && Imaginary.Equals(other.Imaginary);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Complex) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Real, Imaginary);
+        }
+
+        private string PrintMinusOrPlus() => Imaginary >= 0 ? "+" : "-";
+
+        public override string ToString() => $"{Real}{this.PrintMinusOrPlus()}i{Math.Abs(Imaginary)}";
     }
 }
